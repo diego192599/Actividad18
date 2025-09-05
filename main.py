09595e5fe304685c8aae0616f7a541ae1ca31e85
diego192599 <di.cramire@udeo.edu.gt>
2025-09-05 10:12:42 -0600

    clase participantes

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Menu")
+class Participantes:
+    def __init__(self, nombreBanda,institucion,categoria):
+        self.nombreBanda=nombreBanda
+        self.institucion=institucion
+        self.categoria=categoria
+
+    def mostrar_info(self):
+        return f"El nombre de la banda es: {self.nombreBanda} de la isntitucion {self.institucion} y con la categoria {self.categoria}"
+
+
